d19c50c11b1c6e7dadd99527293ca97462b23e4e
Aniome <vaganov.kostya2000@yandex.ru>
2024-05-01 23:37:55 +0500

    add root

--- src/main/java/org/BinaryTrees/BinaryTree.java
+++ src/main/java/org/BinaryTrees/BinaryTree.java
@@ -1,4 +1,33 @@
 package org.BinaryTrees;
 
-public class BinaryTree {
+import org.w3c.dom.Node;
+
+public class BinaryTree<T> {
+    private TreeNode<T> root;
+    private int depth;
+    private int size;
+
+    public void add(T data) {
+        if (size == 0) {
+            add(data, null);
+        } else {
+            add(data, root);
+        }
+        size++;
+    }
+
+    private TreeNode<T> add(T data, TreeNode<T> node) {
+        if (node == null) {
+            root = new TreeNode<>();
+            root.data = data;
+            root.left = null;
+            root.right = null;
+            root.parent = null;
+            depth++;
+            return root;
+        }
+//      if (data < node.data) {}
+        return node;
+    }
+
 }
--- src/main/java/org/BinaryTrees/TreeNode.java
+++ src/main/java/org/BinaryTrees/TreeNode.java
@@ -1,4 +1,8 @@
 package org.BinaryTrees;
 
-public class TreeNode {
+public class TreeNode<T> {
+    T data;
+    TreeNode<T> left;
+    TreeNode<T> right;
+    TreeNode<T> parent;
 }
